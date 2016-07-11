1cdca27822ea585947c93d60bfefff4eaf7fe7f3
abhilashsajeev <abhilash@qburst.com>
2016-07-11 10:50:00 +0530

    Fixed highlighter issue after scroll

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.body.addEventListener('mouseup', function(e) {
     var selectedText = window.getSelection();
     var textCoordinates = selectedText.getRangeAt(0).getBoundingClientRect();
     popup.classList.toggle('highlightMenu--active');
-    popup.style.top = (textCoordinates.top - popup.getBoundingClientRect().height - 8) + 'px';
+    popup.style.top = (textCoordinates.top - popup.getBoundingClientRect().height - 8 + window.scrollY) + 'px';
     popup.style.left = (textCoordinates.left + textCoordinates.width / 2 - popup.getBoundingClientRect().width / 2) + 'px';
   } else {
     popup.classList.remove('highlightMenu--active');
